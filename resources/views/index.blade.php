282d31926b3a7472c51cd15e5273c125c1da8544
GrandCore Admin <54822665+grandcoreadmin@users.noreply.github.com>
2020-07-21 16:14:42 -0300

    text-fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -211,7 +211,7 @@
             </div>
             <div class="col-12 col-md-4">
                 <div class="inner">
-                    <h3>Напишите Нам</h3>
+                    <h2 class="color:red;">Напиши нам, если хочешь нам помочь или у тебя есть вопросы и предложения.</h2>
                     <ul>
                         <li>Telegram - <a href="https://t.me/i0zgMRV49fX">@i0zgMRV49fX</a></li>
                         <li>E-mail - <a href="mailto:ru@grandcore.org">ru@grandcore.org</a></li>
@@ -263,7 +263,7 @@
                 <a data-fancybox href="https://www.youtube.com/watch?v=dv1QFTSCrHo">
                     <span class="text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="110px" height="110px"><path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"/><path fill="#FFF" d="M20 31L20 17 32 24z"/></svg><br />
-                        Посмотреть видео презентации проекта
+                        <b>Посмотреть видео презентации проекта</b>
                     </span>
                 </a>
             </div>
